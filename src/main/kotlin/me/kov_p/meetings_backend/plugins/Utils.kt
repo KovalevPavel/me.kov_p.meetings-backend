b2370a17260c8312552df0bfe9810b6e4e5d2265
Kovalev Pavel <kov_p@mail.ru>
2022-07-17 23:53:08 +0300

    trying fix deserialization

--- src/main/kotlin/me/kov_p/meetings_backend/plugins/Utils.kt
+++ src/main/kotlin/me/kov_p/meetings_backend/plugins/Utils.kt
@@ -2,12 +2,16 @@ package me.kov_p.meetings_backend.plugins
 
 import com.google.gson.Gson
 import io.ktor.server.application.ApplicationCall
+import io.ktor.server.request.receive
+import io.ktor.server.request.receiveStream
 import io.ktor.server.request.receiveText
 import kotlin.reflect.KClass
 
 internal suspend inline fun <reified T : Any> ApplicationCall.parseResponse(clazz: KClass<T>): T {
-    val respond = this.receiveText()
-
+    val respond = this.receiveStream().bufferedReader().use {
+        it.readText()
+    }
+    println("respond -> $respond")
     return try {
         Gson().fromJson(
             respond,
