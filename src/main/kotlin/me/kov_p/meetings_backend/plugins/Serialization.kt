e18da659cdd11ca5e7c5a0860221788188e99e24
Kovalev Pavel <kov_p@mail.ru>
2022-07-18 00:13:44 +0300

    trying fix deserialization

--- src/main/kotlin/me/kov_p/meetings_backend/plugins/Serialization.kt
+++ src/main/kotlin/me/kov_p/meetings_backend/plugins/Serialization.kt
@@ -4,9 +4,18 @@ import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(json = Json {
+            encodeDefaults = true
+            isLenient = true
+            allowSpecialFloatingPointValues = true
+            allowStructuredMapKeys = true
+            prettyPrint = false
+            useArrayPolymorphism = false
+            ignoreUnknownKeys = true
+        })
     }
 }
