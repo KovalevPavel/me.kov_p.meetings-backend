f0aa89e30251b1d5493b42ebc1da9d92b2efb935
Kovalev Pavel <kov_p@mail.ru>
2022-07-23 10:09:26 +0300

    fixed serialization

--- src/main/kotlin/me/kov_p/meetings_backend/database/models/UserDto.kt
+++ src/main/kotlin/me/kov_p/meetings_backend/database/models/UserDto.kt
@@ -1,8 +1,5 @@
 package me.kov_p.meetings_backend.database.models
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class UserDto(
     val userName: String,
     val chatId: Long,
