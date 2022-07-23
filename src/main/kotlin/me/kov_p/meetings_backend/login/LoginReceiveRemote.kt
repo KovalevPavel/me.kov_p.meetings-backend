5f550c2881f1704601a50e1f926a067fc26e0a86
Kovalev Pavel <kov_p@mail.ru>
2022-07-23 10:17:10 +0300

    fixed serialization, updated .gitignore

--- src/main/kotlin/me/kov_p/meetings_backend/login/LoginReceiveRemote.kt
+++ src/main/kotlin/me/kov_p/meetings_backend/login/LoginReceiveRemote.kt
@@ -1,8 +1,5 @@
 package me.kov_p.meetings_backend.login
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class LoginReceiveRemote(
     val userName: String?,
 )
--- src/main/kotlin/me/kov_p/meetings_backend/login/LoginRespondRemote.kt
+++ src/main/kotlin/me/kov_p/meetings_backend/login/LoginRespondRemote.kt
@@ -1,8 +1,5 @@
 package me.kov_p.meetings_backend.login
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class LoginRespondRemote(
     val token: String
 )
