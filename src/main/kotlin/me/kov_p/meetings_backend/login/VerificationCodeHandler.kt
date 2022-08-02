02ac60b73c98d61796cb4ab18612661bce39cec0
Kovalev Pavel <kov_p@mail.ru>
2022-08-02 23:27:22 +0300

    testing generating code

--- src/main/kotlin/me/kov_p/meetings_backend/login/VerificationCodeHandler.kt
+++ src/main/kotlin/me/kov_p/meetings_backend/login/VerificationCodeHandler.kt
@@ -11,19 +11,23 @@ class VerificationCodeHandlerImpl : VerificationCodeHandler {
     private val loginList = mutableSetOf<UserLoginData>()
 
     override fun requestLoginCode(userLogin: String): Int? {
+        println("handler hash -> ${this.hashCode()}")
         return when (canGenerateCode(userLogin = userLogin)) {
             false -> {
                 null
             }
             else -> {
+                println("list -> $loginList")
                 Random.nextInt(MIN_CODE_VALUE, MAX_CODE_VALUE)
                     .let { loginCode ->
-                        UserLoginData(
-                            userLogin = userLogin,
-                            generatedCode = loginCode,
-                            createdTime = System.currentTimeMillis(),
+                        loginList.add(
+                            UserLoginData(
+                                userLogin = userLogin,
+                                generatedCode = loginCode,
+                                createdTime = System.currentTimeMillis(),
+                            )
                         )
-                            .let(loginList::add)
+                        println("list after add -> $loginList")
                         loginCode
                     }
             }
@@ -35,7 +39,9 @@ class VerificationCodeHandlerImpl : VerificationCodeHandler {
     }
 
     private fun canGenerateCode(userLogin: String): Boolean {
+        println("finding user data...")
         val userData = loginList.firstOrNull { it.userLogin == userLogin } ?: return true
+        println("user data -> $userData\ncurrent time = ${System.currentTimeMillis()}")
         return userData.createdTime <= System.currentTimeMillis() + NEW_CODE_GENERATE_DELAY_MS
     }
 
